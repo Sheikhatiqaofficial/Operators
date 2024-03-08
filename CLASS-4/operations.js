d85c4974822a164c6f30370ce376b72506b2d750
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-03-09 03:00:43 +0500

    Add files via upload

---
+++ CLASS-4/operations.js
@@ -0,0 +1,25 @@
+// ADDITION(+):
+var value1 = 6;
+var value2 = 9;
+var basket = value1 + value2;
+console.log(basket);
+// SUBTRACTION(-):
+var value3 = 29;
+var value4 = 9;
+var total = value3 - value4;
+console.log(total);
+// MULTIPLICATION(*):
+var value5 = 6;
+var value6 = 5;
+var totalvalue = value5 * value6;
+console.log(totalvalue);
+// DIVISION(/):
+var value7 = 40;
+var value8 = 4;
+var each = value7 / value8;
+console.log(each);
+// EXPONENTIATION(**):
+var layer = 9;
+var melon = 9;
+var power = Math.pow(layer, 2);
+console.log(power);
---
+++ CLASS-4/operations.ts
@@ -0,0 +1,30 @@
+// ADDITION(+):
+
+let value1: number = 6
+let value2: number = 9
+let basket: number = value1 + value2
+console.log(basket);
+// SUBTRACTION(-):
+
+let value3: number = 29
+let value4: number = 9
+let total: number = value3 - value4
+console.log(total);
+// MULTIPLICATION(*):
+
+let value5: number = 6
+let value6: number = 5
+let totalvalue: number = value5 * value6
+console.log(totalvalue);
+// DIVISION(/):
+
+let value7: number = 40
+let value8: number = 4
+let each: number = value7 / value8
+console.log(each);
+// EXPONENTIATION(**):
+
+let layer : number = 9
+let melon : number = 9
+let power: number = layer **2
+console.log(power);
